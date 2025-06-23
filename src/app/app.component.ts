61448a7d111251b2e5604d1d6e5b0ca33e34d201
Mateus Dias <mateuussilvapb@gmail.com>
2025-06-23 13:21:12 -0300

    [feature] [sp02] [mssousa] - Configurações do PrimeNG: configurações básicas, importação de estilo de fonte padrão, configurações de tradução e exemplo de mudança de tema (dark/light).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+//Angular
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//Externos
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DatePickerModule } from 'primeng/datepicker';
+
+//Internos
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    //Angular
+    RouterOutlet,
+
+    //Externos
+    CardModule,
+    DatePickerModule,
+    ButtonModule,
+
+    //Internos
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('app_jm_perfumaria');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+//Angular
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+//Externos
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+
+//Internos
+import { routes } from './app.routes';
+import { Opcoes } from './config/primeNG/traducao.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.app_jm_perfumaria',
+          cssLayer: false,
+        },
+      },
+      ripple: false,
+      translation: Opcoes.traducaoPtBr,
+    }),
   ],
 };
