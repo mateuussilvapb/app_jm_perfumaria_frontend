f108792e56c67cdb83b27ec822f963be3407aa03
Mateus Dias <mateuussilvapb@gmail.com>
2025-07-13 12:44:14 -0300

    [feature] [sp02] [mssousa] Exportando componentes shared em um único arquivo

---
+++ src/app/shared/shared-components.ts
@@ -0,0 +1,8 @@
+import { LoadingComponent } from "./components/loading/loading.component";
+import { FormControlErrorsComponent } from "./components/form-control-errors/form-control-errors.component";
+
+
+export const SHARED_COMPONENTS = [
+  LoadingComponent,
+  FormControlErrorsComponent,
+]
