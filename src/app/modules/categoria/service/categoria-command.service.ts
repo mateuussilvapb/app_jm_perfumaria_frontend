5f68953ebd9cbbb4f2b5fdad4de953a3d00b94c5
Mateus Dias <mateuussilvapb@gmail.com>
2025-07-17 10:27:24 -0300

    [feature] [sp02] [mssousa] Criação do command Service de categoria

---
+++ src/app/modules/categoria/service/categoria-command.service.ts
@@ -0,0 +1,22 @@
+//Angular
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//Internos
+import { Categoria } from '@categoria/interfaces/categoria';
+import { AbstractCommandService } from '@shared/services/abstract-command.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriaCommandService extends AbstractCommandService<Categoria> {
+  protected override path = (): string => 'categorias';
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  toogleStatus = (id: string): Observable<Categoria> =>
+    this.http.patch<Categoria>(`${this.baseURL}/command/toogleStatus/${id}`, null);
+}
