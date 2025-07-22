fe195954b7faea0647303f23b228d33cbcfc8468
Mateus Dias <mateuussilvapb@gmail.com>
2025-07-22 07:36:25 -0300

    [feature] [sp02] [mssousa] CRUD de Categoria finalizado

--- src/app/modules/categoria/components/context-menu-categoria/context-menu-categoria.component.ts
+++ src/app/modules/categoria/components/context-menu-categoria/context-menu-categoria.component.ts
@@ -90,6 +90,7 @@ export class ContextMenuCategoria extends ContextMenu<ContextMenuCategoriaData> 
         'Tem certeza que deseja excluir esta categoria? A ação não poderá ser desfeita.',
       header: 'Confirma?',
       icon: 'pi pi-exclamation-triangle',
+      rejectButtonStyleClass: 'p-button-secondary',
       acceptButtonStyleClass: 'p-button-danger',
       accept: () => this.excluir(idString),
     });
@@ -100,7 +101,7 @@ export class ContextMenuCategoria extends ContextMenu<ContextMenuCategoriaData> 
       this.messageService.add({
         severity: 'success',
         summary: 'Sucesso',
-        detail: 'Categoria excluído com sucesso.',
+        detail: 'Categoria excluída com sucesso.',
         life: 5000,
       });
       this.refresh$.next();
