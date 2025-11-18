df28c7747288ed7ffb484fde130b557cd4d866f4
Mateus Dias <mateuussilvapb@gmail.com>
2025-11-18 16:08:52 -0300

    [feature] [sp02] [mssousa] ajustes

--- src/environments/environment.localhost.ts
+++ src/environments/environment.localhost.ts
@@ -1,11 +1,11 @@
 import * as packageJson from '../../package.json';
 
 export const environment = {
-  apiUrl: 'https://api.18-229-37-145.sslip.io',
+  apiUrl: 'http://localhost:8081',
   postLogoutUrl: '',
   baseHref: '',
   keycloak: {
-    url: 'https://auth.18-229-37-145.sslip.io',
+    url: 'http://localhost:8080',
     realm: 'JMPERFUMARIA',
     clientId: 'app_jm_perfumaria',
     acceptablePaths: [],
